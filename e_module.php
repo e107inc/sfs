e19c3ccbdfaeef77d0d56b4b778020444aafccd0
Cameron <cameron@e107coders.org>
2013-03-13 16:25:01 -0700

    Initial Files

---
+++ e_module.php
@@ -0,0 +1,129 @@
+<?php
+
+if (!defined('e107_INIT')) { exit; }
+
+global $e_event; 
+
+if(is_object($e_event))
+{
+	$e_event->register("usersup_veri", "sfsCheck", e_PLUGIN."sfs/e_module.php");
+}
+
+// Uncomment to Test. 
+//$vals = array('ip'=>"70.32.38.83");
+// var_dump(sfsCheck($vals));
+
+if(!function_exists("sfsCheck"))
+{
+	/**
+	 * Stop Forum Spam module by e107 Inc. using the stopforumspam.com API. 
+	 * @param $val array of user data. 
+	 */
+	function sfsCheck($val=array())
+	{
+		$error 			= false;
+	
+		$user_email 	= trim(varset($val['email']));
+		$user_ip 		= varset($val['ip']) ? trim($val['ip']) : USERIP;
+		$user_name 		= trim(varset($val['loginname']));	 
+
+		if(!is_object("parseXml"))
+		{
+			require_once(e_HANDLER."xml_class.php");	
+		}
+		
+		$xml = new parseXml;
+			
+		// Check IP
+		if ($user_ip != "")
+		{
+			$xml->setUrl("http://www.stopforumspam.com/api?ip=" . urlencode($user_ip));
+			$data = $xml->getRemoteXmlFile();
+			$xm = new SimpleXMLElement($data);
+
+			switch ($xm->appears) 
+		 	{
+				case 'yes':
+					sfsLog($data);
+					return true;  // Is a BOT. 
+				break;
+
+				case 'no': 
+					return false;  
+				break;
+					
+				default:
+					sfsLog(date('r')." : Couldn't check stopforumspam.com against ".$user_ip);
+					return false;  
+				break;
+			 } 
+		  }
+	
+		// Check Email 
+		if ($user_email != "")
+		{
+			$xml->setUrl("http://www.stopforumspam.com/api?email=" . urlencode($user_email));
+			$data = $xml->getRemoteXmlFile();
+			$xm = new SimpleXMLElement($data);
+
+			switch ($xm->appears) 
+		 	{
+				case 'yes': 
+					sfsLog($data);
+					return true; 	   // Is a BOT. 
+				break;
+
+				case 'no': 
+					return false;  
+				break;
+				
+				default:
+					sfsLog(date('r')." : Couldn't check stopforumspam.com against ".$user_email);
+					return false;  
+				break;
+			} 
+		}	
+
+		// Check username  
+		if ($user_name != "")
+		{
+			$xml->setUrl("http://www.stopforumspam.com/api?username==" . urlencode($user_name));
+			$data = $xml->getRemoteXmlFile();
+			$xm = new SimpleXMLElement($data);
+
+				switch ($xm->appears) 
+		 		{
+					case 'yes': 
+						sfsLog($data);
+						return true; 	   // Is a BOT. 
+					break;
+
+					case 'no': 
+						return false;  
+					break;
+				
+					default:
+						sfsLog(date('r')." : Couldn't check stopforumspam.com against ".$user_name);
+						return false;  
+					break;
+			} 
+		}	
+		
+		
+		
+	}
+
+	// Log Raw Data 
+	function sfsLog($data)
+	{
+		$path = (defined("e_LOG")) ? e_LOG."sfs.log" : e_PLUGIN."sfs/sfs.log";	
+		$data .= "\n\n";
+		@file_put_contents($path, $data, FILE_APPEND | LOCK_EX);	
+	}
+	
+}
+
+
+
+
+?>
---
+++ plugin.php
@@ -0,0 +1,48 @@
+<?php
+/*
+*************************************
+e107 Inc. 
+*/
+if (!defined('e107_INIT')) { exit; }
+
+// Plugin info  
+$eplug_name    = "Stop Forum Spam";
+$eplug_version = "1.0";
+$eplug_author  = "e107 inc";
+$eplug_url = "http://e107.org";
+$eplug_email = "nospam@e107.org";
+
+$eplug_description = "Based on the work of stopforumspam.com";
+$eplug_compatible  = "e107 v0.7+";
+$eplug_readme      = "";
+$eplug_status = false;
+
+// Name of the plugin's folder
+$eplug_folder = "sfs";
+
+// Name of menu item for plugin  
+$eplug_menu_name	= "";
+
+// Name of the admin configuration file  
+$eplug_conffile = "admin_config.php";
+
+// Icon image and caption text
+$eplug_icon = $eplug_folder."/images/icon_32.png";
+$eplug_icon_small = $eplug_folder."/images/icon_16.png";
+$eplug_logo = $eplug_folder."/images/icon_32.png";
+$eplug_caption = "Stop Forum Spam";
+
+// Create a link in main menu (yes=TRUE, no=FALSE) 
+$eplug_link = false;
+$eplug_link_url		= "";
+$eplug_link_name    = "";
+
+// Text to display after plugin successfully installed 
+$eplug_done           = "Installation Successful..";
+$eplug_uninstall_done = "Uninstalled Successfully..";
+
+// List of sql requests to create tables 
+$eplug_tables = array(); 
+
+
+?>
