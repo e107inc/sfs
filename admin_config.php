495d68a85dfc861d76e316b715650fb8aa0acfa4
Moc <github@tijnkuyper.nl>
2021-01-23 16:01:13 +0100

    Now using Admin-UI. Prepared for API key usage.

--- admin_config.php
+++ admin_config.php
@@ -8,76 +8,185 @@
  *
  */
 
-// TODO REWRITE TO USE ADMIN UI
-
-require_once("../../class2.php");
-
-if (!getperms("P"))
+require_once(__DIR__.'/../../class2.php');
+if (!getperms('P')) 
 {
-	 header("location:" . e_BASE."index.php");  
-	 exit;
+	e107::redirect('admin');
+	exit;
 }
+
+e107::lan('sfs', true, true);
+
+class sfs_adminArea extends e_admin_dispatcher
+{
+	protected $modes = array(	
+		'main'	=> array(
+			'controller' 	=> 'sfs_ui',
+			'path' 			=> null,
+			'ui' 			=> 'sfs_form_ui',
+			'uipath' 		=> null
+		),
+	);	
+	
+	
+	protected $adminMenu = array(
+		//'main/list'			=> array('caption'=> LAN_MANAGE, 'perm' => 'P'),
+		//'main/create'		=> array('caption'=> LAN_CREATE, 'perm' => 'P'),
+
+		'main/prefs' 		=> array('caption'=> LAN_PREFS, 'perm' => 'P'),	
+
+		// 'main/div0'      => array('divider'=> true),
+		// 'main/custom'		=> array('caption'=> 'Custom Page', 'perm' => 'P'),
+	);
+
+	protected $adminMenuAliases = array(
+		'main/edit'	=> 'main/list'				
+	);	
+	
+	protected $menuTitle = 'Stop Forum Spam';
+}
+
+	
+class sfs_ui extends e_admin_ui
+{
+		protected $pluginTitle		= 'Stop Forum Spam';
+		protected $pluginName		= 'sfs';
+	//	protected $eventName		= 'sfs-sfs'; // remove comment to enable event triggers in admin. 		
+		protected $table			= 'sfs';
+		protected $pid				= '';
+		protected $perPage			= 10; 
+		protected $batchDelete		= false;
+		protected $batchExport      = false;
+		protected $batchCopy		= false;
+
+	//	protected $sortField		= 'somefield_order';
+	//	protected $sortParent       = 'somefield_parent';
+	//	protected $treePrefix       = 'somefield_title';
+
+	//	protected $tabs				= array('Tabl 1','Tab 2'); // Use 'tab'=>0  OR 'tab'=>1 in the $fields below to enable. 
+		
+		protected $listOrder		= '';
+	
+		protected $fields = array();		
+		
+		protected $fieldpref = array();
+		
+	//	protected $preftabs        = array('General', 'Other' );
+		protected $prefs = array(
+			'sfs_enabled' => array(
+				'title'			=> LAN_SFS_PREFS_ACTIVE, 
+				'tab'			=> 0, 
+				'type'			=> 'boolean', 
+				'data' 			=> 'int', 
+				'help'			=> LAN_SFS_PREFS_ACTIVE_HELP, 
+				'writeParms'	=> array()
+			),
+			'sfs_debug' => array(
+				'title'			=> LAN_SFS_PREFS_DEBUG, 
+				'tab'			=> 0, 
+				'type'			=> 'boolean', 
+				'data' 			=> 'int', 
+				'help'			=> LAN_SFS_PREFS_DEBUG_HELP, 
+				'writeParms'	=> array()
+			),
+			'sfs_apikey' => array(
+				'title'			=> LAN_SFS_PREFS_APIKEY, 
+				'tab'			=> 0,
+				'type'			=> 'text', 
+				'data' 			=> 'str', 
+				'help'			=> LAN_SFS_PREFS_APIKEY_HELP, 
+				'writeParms' 	=> array()
+			),
+		); 
+
+	
+		public function init()
+		{
+			// This code may be removed once plugin development is complete. 
+			if(!e107::isInstalled('sfs'))
+			{
+				e107::getMessage()->addWarning("This plugin is not yet installed. Saving and loading of preference or table data will fail."); // DO NOT TRANSLATE
+			}
+		}
+
+		
+		// ------- Customize Create --------
+		
+		public function beforeCreate($new_data,$old_data)
+		{
+			return $new_data;
+		}
+	
+		public function afterCreate($new_data, $old_data, $id)
+		{
+			// do something
+		}
+
+		public function onCreateError($new_data, $old_data)
+		{
+			// do something		
+		}		
+		
+		
+		// ------- Customize Update --------
+		
+		public function beforeUpdate($new_data, $old_data, $id)
+		{
+			return $new_data;
+		}
+
+		public function afterUpdate($new_data, $old_data, $id)
+		{
+			// do something	
+		}
+		
+		public function onUpdateError($new_data, $old_data, $id)
+		{
+			// do something		
+		}		
+		
+		// left-panel help menu area
+		public function renderHelp()
+		{
+			$text = '';
+			$caption = LAN_HELP;
+
+			if($this->getAction() == "prefs")
+			{			
+				$text .= '<strong>'.LAN_SFS_PREFS_DEBUG.'</strong>'; 
+				$text .= '<p>'.LAN_SFS_PREFS_DEBUG_HELP.'</p>';
+
+				$text .= '<strong>'.LAN_SFS_PREFS_APIKEY.'</strong>'; 
+				$text .= '<p>'.LAN_SFS_PREFS_APIKEY_HELP.'</p>';
+			}
+			
+
+			return array('caption' => $caption,'text' => $text);
+		}
+			
+		/*	
+		// optional - a custom page.  
+		public function customPage()
+		{
+			$text = 'Hello World!';
+			$otherField  = $this->getController()->getFieldVar('other_field_name');
+			return $text;
+			
+		}
+		*/			
+}
+				
+
+class twofactorauth_form_ui extends e_admin_form_ui
+{
+	
+
+}				
+		
+new sfs_adminArea();
 
 require_once(e_ADMIN."auth.php");
-
-
-class sfs
-{
-	
-	function render()
-	{
-		global $pref, $ns;
-		
-		if(varset($_POST['sfs_save']))
-		{
-			$pref['sfs_enabled'] = intval($_POST['sfs_enabled']);
-			$pref['sfs_debug'] = intval($_POST['sfs_debug']);
-			$ns->tablerender("Saved","Settings Saved");
-			save_prefs();
-		}
-				
-		$text .= $this->prefsForm();		
-		return $text;
-	}
-	
-	
-	function prefsForm()
-	{
-		global $sql,$sql2,$tp,$pref;
-		
-		
-		$checked = ($pref['sfs_enabled'] == 1) ? "checked='checked'" : "";
-		$checkeddbg = ($pref['sfs_debug'] == 1) ? "checked='checked'" : "";
-		
-		$text .= "
-		<form method='post' action='".e_SELF."' >
-		<label style='display:block'>
-			<input type='checkbox' name='sfs_enabled' value='1' {$checked} /> Enable the <em>Stop Forum Spam</em> system 
-			
-		</label>
-		<label style='display:block'>
-			<input type='checkbox' name='sfs_debug' value='1' {$checkeddbg} /> Enable logging of all results<br /><small>(those found to be spam will be logged by default)</small>. 	
-		</label>
-		
-		<div style='padding:10px'>
-			<input type='submit' class='btn btn-primary button' name='sfs_save' value='Save' />
-		</div>
-		</form>";
-		
-		return $text;
-	}
-	
-
-}
-
-$sfs = new sfs;
-
-$ns -> tablerender("Stop Forum Spam", $sfs->render());
-
-
+e107::getAdminUI()->runPage();
 
 require_once(e_ADMIN."footer.php");
-
-
-
-?>
+exit;
---
+++ languages/English/English_admin.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * StopForumSpam (SFS)
+ *
+ * Copyright (C) 2021-2022 e107 Inc. (https://www.e107.org)
+ * Released under the terms and conditions of the
+ * GNU General Public License (http://www.gnu.org/licenses/gpl.txt)
+ *
+ */
+
+// Prefs
+define("LAN_SFS_PREFS_ACTIVE", 			"StopForumSpam Active");
+define("LAN_SFS_PREFS_ACTIVE_HELP", 	"Either enables or disable StopForumSpam.");
+
+define("LAN_SFS_PREFS_DEBUG", 			"SFS Debug Mode");
+define("LAN_SFS_PREFS_DEBUG_HELP", 		"Enable logging of ALL results. Those found to be spam are already logged by default.");
+
+define("LAN_SFS_PREFS_APIKEY", 			"API Key");
+define("LAN_SFS_PREFS_APIKEY_HELP", 	"This key is used to connect to the stopforumspam.com database (optional)");
+
+
+// Help
+define("LAN_SFS_HELP_DEBUG", 	"By default, user registrations found to be spam are logged to the log file. By enabling debug mode, ALL checks are logged, including the legitimate registrations.");
+define("LAN_SFS_HELP_APIKEY", 	"When set...");
