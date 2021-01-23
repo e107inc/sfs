221014b80b4ac9bdff50133288e6c67e290cfb29
Moc <github@tijnkuyper.nl>
2021-01-23 16:41:04 +0100

    Get rid of e_module. Refactor new SFS class.

--- sfs_class.php
+++ sfs_class.php
@@ -36,52 +36,45 @@ class sfs_class
 
 	function sfsCheck($val = array())
 	{
+		$xml = e107::getXml(); 
+
 		// Check if SFS is active (just making sure)
 		if(!e107::getPlugPref('sfs', 'sfs_enabled')) 
 		{
 			return false; 
 		}	
-
 	
 		$user_email 	= trim(varset($val['email']));
 		$val['ip'] 		= varset($val['ip']) ? trim($val['ip']) : USERIP;
 		$user_name 		= trim(varset($val['loginname']));	 
 		
 		$deniedMessage = "Sorry no bots allowed!";
-
-		if(!is_object("parseXml"))
-		{
-			require_once(e_HANDLER."xml_class.php");	
-		}
-		
-		$xml = new parseXml;
 			
 		// Check IP
 		if ($val['ip']  != "")
 		{
-			$xml->setUrl("http://www.stopforumspam.com/api?ip=" . urlencode($val['ip'] ));
-			if(!$data = $xml->getRemoteXmlFile())
+			if(!$data = $xml->getRemoteFile("http://www.stopforumspam.com/api?ip=" . urlencode($val['ip'] )))
 			{
-				sfsLog(date('r')." : Couldn't access stopforumspam.com");
+				$this->sfsLog(date('r')." : Couldn't access stopforumspam.com");
 				return;
 			}
-			
+		
 			$xm = new SimpleXMLElement($data);
-
-			switch ($xm->appears) 
+			
+			switch($xm->appears) 
 		 	{
 				case 'yes':
-					sfsLog($data, $val);
+					$this->sfsLog($data, $val);
 					return $deniedMessage;  // Is a BOT. 
 				break;
 
 				case 'no': 
-					sfsLog($data, $val , false);
+					$this->sfsLog($data, $val , false);
 					return false;  
 				break;
 					
 				default:
-					sfsLog(date('r')." : Couldn't check stopforumspam.com against ". $val['ip'] , $val);
+					$this->sfsLog(date('r')." : Couldn't check stopforumspam.com against ". $val['ip'] , $val);
 					return false;  
 				break;
 			 } 
@@ -90,85 +83,80 @@ class sfs_class
 		// Check Email 
 		if ($user_email != "")
 		{
-			$xml->setUrl("http://www.stopforumspam.com/api?email=" . urlencode($user_email));
-			if(!$data = $xml->getRemoteXmlFile())
+			if(!$data = $xml->getRemoteXmlFile("http://www.stopforumspam.com/api?email=" . urlencode($user_email)))
 			{
-				sfsLog(date('r')." : Couldn't access stopforumspam.com");
+				$this->sfsLog(date('r')." : Couldn't access stopforumspam.com");
 				return;
 			}
+
 			$xm = new SimpleXMLElement($data);
 
 			switch ($xm->appears) 
 		 	{
 				case 'yes': 
-					sfsLog($data,$val);
+					$this->sfsLog($data,$val);
 					return $deniedMessage; 	   // Is a BOT. 
 				break;
 
 				case 'no': 
-					sfsLog($data, $val, false);
+					$this->sfsLog($data, $val, false);
 					return false;  
 				break;
 				
 				default:
-					sfsLog(date('r')." : Couldn't check stopforumspam.com against ".$user_email, $val);
+					$this->sfsLog(date('r')." : Couldn't check stopforumspam.com against ".$user_email, $val);
 					return false;  
 				break;
 			} 
-		}	
+		}
 
 		// Check username  
-		if ($user_name != "")
+		if($user_name != "")
 		{
-			$xml->setUrl("http://www.stopforumspam.com/api?username==" . urlencode($user_name));
-			if(!$data = $xml->getRemoteXmlFile())
+			if(!$data = $xml->getRemoteXmlFile("http://www.stopforumspam.com/api?username==" . urlencode($user_name)))
 			{
-				sfsLog(date('r')." : Couldn't access stopforumspam.com");
+				$this->sfsLog(date('r')." : Couldn't access stopforumspam.com");
 				return;
 			}
+
 			$xm = new SimpleXMLElement($data);
 
 				switch ($xm->appears) 
 		 		{
 					case 'yes': 
-						sfsLog($data,$val);
+						$this->sfsLog($data,$val);
 						return $deniedMessage; 	   // Is a BOT. 
 					break;
-
 					case 'no': 
-						sfsLog($data, $val, false);
+						$this->sfsLog($data, $val, false);
 						return false;  
 					break;
-				
 					default:
-						sfsLog(date('r')." : Couldn't check stopforumspam.com against ".$user_name, $val);
+						$this->sfsLog(date('r')." : Couldn't check stopforumspam.com against ".$user_name, $val);
 						return false;  
 					break;
 			} 
-		}	
+		}
+	}
 
-		// TODO Rewrite sfsCheck() to use e107::getAdminLog()->addDebug() and toFile(). 
-		// Log Raw Data 
-		function sfsLog($data,$val, $status=true)
+	// TODO Rewrite sfsCheck() to use e107::getAdminLog()->addDebug() and toFile(). 
+	// Log Raw Data 
+	function sfsLog($data, $val, $status=true)
+	{
+		global $pref; 
+		
+		if($status == false && ($pref['sfs_debug'] != 1))
 		{
-			global $pref; 
-			
-			if($status == false && ($pref['sfs_debug'] != 1))
-			{
-				return; 	
-			}
-			
-			$path = (defined("e_LOG")) ? e_LOG."sfs.log" : e_PLUGIN."sfs/sfs.log";	
-			
-			$save = date('r')."\nUSERNAME: ".$val['loginname']." EMAIL: ".$val['email']." IP: ".$val['ip'];
-			$save .= "\n".$data;
-			$save .= "\n\n";
-			
-			@file_put_contents($path, $save, FILE_APPEND | LOCK_EX);
-			@chmod($path,0640);	
+			return; 	
 		}
 		
+		$path = (defined("e_LOG")) ? e_LOG."sfs.log" : e_PLUGIN."sfs/sfs.log";	
 		
+		$save = date('r')."\nUSERNAME: ".$val['loginname']." EMAIL: ".$val['email']." IP: ".$val['ip'];
+		$save .= "\n".$data;
+		$save .= "\n\n";
 		
+		@file_put_contents($path, $save, FILE_APPEND | LOCK_EX);
+		@chmod($path,0640);	
 	}
 }
